c05d859a4a3372732952ec233a55ddc4831ecfa4
escencial <escencial@escencia.local>
2013-07-21 02:46:50 -0300

    delegate Application issues on app.php

--- src/controllers.php
+++ src/controllers.php
@@ -1,24 +1,12 @@
 <?php
 
-$app->get('/', function() 
+$app->get('/', function() use ($app) 
 {
-	Twig_Autoloader::register();
-
- 	$loader = new Twig_Loader_Filesystem(__DIR__.'/../templates');
-/**/
-	$twig = new Twig_Environment($loader, array(
-	    'cache' => __DIR__.'/../cache',
-	));
-/**/
-	$twig->clearCacheFiles();
-
 	$data = array('name' => 'escencial','title' => 'Slx');
-
-	return $twig->render('index.html.twig', $data);
+	return $app['twig']->render('index.html.twig', $data);
 });
 
-$app->get('/clear-cache', function() 
+$app->get('/cc', function() use ($app)
 {
-	
-	return $loader->clearCacheFiles();
+	return $app['twig']->clearCacheFiles();
 });
--- web/index.php
+++ web/index.php
@@ -1,13 +1,10 @@
 <?php
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 
 require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../vendor/twig/twig/lib/Twig/Autoloader.php';
 
-$app = new Silex\Application();
-/**
- * 
- */
+$app = require __DIR__.'/../src/app.php';
+
 require __DIR__.'/../src/controllers.php';
 
 $app->run();
